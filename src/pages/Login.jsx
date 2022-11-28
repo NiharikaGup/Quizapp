3d4f6977bd165f00db870f520e9e3c65c67a5a20
Niharika Gupta <niharika0108@gmail.com>
2022-11-28 08:53:23 +0100

    QUIZ-2 added css to login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,23 @@
+import Button from '@mui/material/Button';
+import './login.css';
+
 function Login() {
     return (
-        <div>
-            <h1>Login</h1>
+        <div className="form public-form-wrap ">
+            <div className='public-form-content login-content'>
+                <h1>Login</h1>
+                <div className="user-name">
+                    <label>Username </label>
+                    <input type="text" name="uname" required />
+                </div>
+                <div className="password">
+                    <label>Password </label>
+                    <input type="password" name="pass" required />
+                </div>
+                <div className="button">
+                <Button variant="contained">Submit</Button>
+                </div>
+            </div>
         </div>
     )
 }
