4e13bc5a6908fe176be15d2f8121def9c1d89b78
Niharika Gupta <niharika0108@gmail.com>
2022-12-22 13:26:51 +0100

    QUIZ-2 created question page UI

--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -1,10 +1,18 @@
 import Box from "@mui/material/Box";
-import Link from "@mui/material/Link";
-import { useNavigate } from "react-router-dom";
 import image from "../images/question4.png";
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
+import Chip from "@mui/material/Chip";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 
 function Questions() {
-  const navigate = useNavigate();
   return (
     <div
       style={{
@@ -24,7 +32,59 @@ function Questions() {
         >
           <div className="public-form-content login-content">
             <div className="page-header" style={{ textAlign: "center" }}>
-              <h1>Question 1/5:</h1>
+              <h3>Question</h3>
+
+              <div style={{ display: "flex", justifyContent: "center" }}>
+                <h5>Subject : 'Subject-name' &nbsp;&nbsp;</h5>
+                <h5>Time Left: HH:MM:SS</h5>
+              </div>
+
+              <FormControl>
+                <FormLabel id="demo-radio-buttons-group-label">
+                  This is my question number one, with four options below.
+                </FormLabel>
+                <RadioGroup
+                  aria-labelledby="demo-radio-buttons-group-label"
+                  defaultValue="female"
+                  name="radio-buttons-group"
+                >
+                  <FormControlLabel
+                    value="option a"
+                    control={<Radio />}
+                    label="a : option a"
+                  />
+                  <FormControlLabel
+                    value="option b"
+                    control={<Radio />}
+                    label="b : option b"
+                  />
+                  <FormControlLabel
+                    value="option c"
+                    control={<Radio />}
+                    label="c : option c"
+                  />
+                  <FormControlLabel
+                    value="option d"
+                    control={<Radio />}
+                    label="d : option d"
+                  />
+                </RadioGroup>
+
+                <Box
+                  sx={{
+                    "& > :not(style)": {
+                      m: 1,
+                    },
+                  }}
+                >
+                  <Chip icon={<ArrowBackIcon />} label="back" />
+                  <Chip icon={<ArrowForwardIcon />} label="next" />
+                  <Chip icon={<HighlightOffIcon />} label="quit quiz" />
+                </Box>
+                <Stack sx={{ alignItems: "center" }}>
+                  <Pagination count={5} color="primary" />
+                </Stack>
+              </FormControl>
             </div>
           </div>
         </Box>
